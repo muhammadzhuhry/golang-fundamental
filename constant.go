f20341bf6da8a9ad6c21e81a03bf47f998a244eb
muhammadzhuhry <muhammadzhuhry@gmail.com>
2020-10-19 08:21:26 +0700

    update file constant.go

--- constant.go
+++ constant.go
@@ -3,9 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	const firstName string = "Muhammad"
-	const lastName = "Zhuhry"
-	const value = 1000
+	const (
+		firstName string = "Muhammad"
+		lastName         = "Zhuhry"
+		value            = 1000
+	)
 
 	//it's okay if you not use constant variable, go will not giving error
 	//fmt.Println(firstName)
