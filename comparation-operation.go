1d24fed01119756f887c44e012928b360decca44
muhammadzhuhry <muhammadzhuhry@gmail.com>
2020-10-19 15:09:01 +0700

    update file comparation-operation.go

--- comparation-operation.go
+++ comparation-operation.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var name1 = "Zuhri"
+	var name2 = "Zuhri"
+
+	var result bool = name1 == name2
+	fmt.Println(result)
+
+	value1, value2 := 100, 200
+
+	fmt.Println(value1 > value2)
+	fmt.Println(value1 < value2)
+	fmt.Println(value1 == value2)
+	fmt.Println(value1 != value2)
 }
