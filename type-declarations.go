c9cf61dc413277454ed4e2773217241ad4ec4c03
muhammadzhuhry <muhammadzhuhry@gmail.com>
2020-10-19 08:43:20 +0700

    update file type-declarations.go

--- type-declarations.go
+++ type-declarations.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	type NoKTP string
+	type Married bool
+
+	var noKtpEko NoKTP = "11090998A"
+	var marriedStatus Married = true
+	fmt.Println(noKtpEko, marriedStatus)
 }
