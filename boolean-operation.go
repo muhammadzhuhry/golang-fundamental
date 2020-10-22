924bbee902eb32d15c65990ed6bd769ac5e7c664
muhammadzhuhry <muhammadzhuhry@gmail.com>
2020-10-22 14:21:02 +0700

    update file boolean-operation.go

--- boolean-operation.go
+++ boolean-operation.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var ujian = 80
+	var absensi = 75
+
+	var kkmUjian = ujian >= 80
+	var kkmAbsensi = absensi >= 80
+
+	var lulus = kkmUjian && kkmAbsensi
+	fmt.Println(lulus)
 }
