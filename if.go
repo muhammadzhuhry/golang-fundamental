b1f6a36cf86bc0e276951ac79f1e66950f23f4f2
muhammadzhuhry <muhammadzhuhry@gmail.com>
2020-10-22 22:16:56 +0700

    update file if.go

--- if.go
+++ if.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var name = "Eko"
+
+	if name == "Eko" {
+		fmt.Println("Hello Eko")
+	} else if name == "Joko" {
+		fmt.Println("Hello Joko")
+	} else {
+		fmt.Println("Hai, Boleh kenalan?")
+	}
+
+	// if short statement
+	if lengthName := len(name); lengthName > 5 {
+		fmt.Println("Nama terlalu panjan")
+	} else {
+		fmt.Println("Nama sudah sesuai")
+	}
 }
