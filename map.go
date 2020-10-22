19d81764eb850d2279d49f97af02b7f41365a835
muhammadzhuhry <muhammadzhuhry@gmail.com>
2020-10-22 21:46:01 +0700

    create file map.go

---
+++ map.go
@@ -0,0 +1,8 @@
+package main
+
+//Map adalah kumpulan key value yang dimana key nya bersifat unik tidak boleh sama dan valuenya boleh tipe data apa saja / Object di javascript
+//Bila key nya sama maka otomatis key data sebelumnya akan di replace dgn key data yang baru
+
+func main() {
+
+}
