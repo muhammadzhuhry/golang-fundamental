4b81535ccfd3c7310783b8cd6af18b60552e72e6
muhammadzhuhry <muhammadzhuhry@gmail.com>
2020-10-22 14:55:21 +0700

    update file slice.go

--- slice.go
+++ slice.go
@@ -1,5 +1,14 @@
 package main
 
+//Tipe data Slice adalah potongan dari data Array
+//Slice mirip dengan Array, yang membedakan adalah ukuran Slice bisa berubah
+//Slice dan Array selalu terkoneksi, dimana Slice adalah data yang mengakses sebagian atau seluruh data di Array
+
+//Tipe data Slice memiliki 3 data, yaitu pointer, length dan capacity
+//Pointer adalah penunjuk data pertama di Array pada Slice
+//Length adalah panjang dari Slice
+//Capacity adalah kapasitas dari Slice, dimana Length tidak boleh lebih dari Capacity
+
 func main() {
 
 }
