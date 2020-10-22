35a8992dd2239872e43a9c8540148e9aeb948c62
muhammadzhuhry <muhammadzhuhry@gmail.com>
2020-10-22 14:25:23 +0700

    tipe data array

--- array.go
+++ array.go
@@ -1,5 +1,9 @@
 package main
 
+//Array harus berisikan kumpulan data dengan tipe yg sama
+//Ketika membuat array, jumlah data yang bisa ditampung harus ditentukan
+//Daya tampung Array tidak bisa bertambah setelah Array dibuat
+
 func main() {
 
 }
