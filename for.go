bd76ff18a6d4934f6887fc08e63e9b31b85cc462
muhammadzhuhry <muhammadzhuhry@gmail.com>
2020-10-26 00:17:10 +0700

    update file for.go

--- for.go
+++ for.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	counter := 1
+
+	for counter <= 10 {
+		fmt.Println("Perulangan ke", counter)
+		counter++
+	}
+
+	// for with statement
+	for i := 1; i <= 10; i++ {
+		fmt.Println(i)
+	}
+
+	slice := []string{"Muhammad", "Athallah", "Zhuhry"}
+
+	for x := 0; x < len(slice); x++ {
+		fmt.Println(slice[x])
+	}
+
+	// for range -> short way untuk tipe data collection (array, slice, map)
+	for index, value := range slice {
+		fmt.Println("index", index, "=", value)
+	}
+
+	//developer := make(map[string]string)
+	developer := map[string]string{}
+	developer["name"] = "Muhammad Athallah Zhuhry"
+	developer["role"] = "Backend Developer"
+
+	for key, value := range developer {
+		fmt.Println(key, "=", value)
+	}
 }
