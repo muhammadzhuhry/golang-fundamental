fbc293bc2f5f92e5ea4128a7c11beb189f53e225
muhammadzhuhry <muhammadzhuhry@gmail.com>
2020-10-26 11:53:09 +0700

    update file continue.go

--- continue.go
+++ continue.go
@@ -1,6 +1,13 @@
 package main
 
+import "fmt"
+
 // Continue -> digunakan untuk menghentikan perulangan yang berjalan dan langsung melanjutkan ke perulangan selanjutnya
 func main() {
-
+	for i := 0; i < 10; i++ {
+		if i%2 == 0 {
+			continue
+		}
+		fmt.Println("Perulangan ke ", i)
+	}
 }
