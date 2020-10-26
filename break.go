d52dbace69f72c25e04ab7de68dfbb8a29dbe20b
muhammadzhuhry <muhammadzhuhry@gmail.com>
2020-10-26 11:51:24 +0700

    update file break.go

--- break.go
+++ break.go
@@ -3,7 +3,6 @@ package main
 import "fmt"
 
 // Break -> digunakan untuk menghentikan seluruh perulangan
-// Continue -> digunakan untuk menghentikan perulangan yang berjalan dan langsung melanjutkan ke perulangan selanjutnya
 func main() {
 	for i := 0; i < 10; i++ {
 		fmt.Println("Perulangan ke ", i)
