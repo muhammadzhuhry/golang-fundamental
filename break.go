15f427bbf266f7517b24fdd3880a0ffd02e66884
muhammadzhuhry <muhammadzhuhry@gmail.com>
2020-10-26 11:50:06 +0700

    update file break.go

--- break.go
+++ break.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import "fmt"
 
+// Break -> digunakan untuk menghentikan seluruh perulangan
+// Continue -> digunakan untuk menghentikan perulangan yang berjalan dan langsung melanjutkan ke perulangan selanjutnya
+func main() {
+	for i := 0; i < 10; i++ {
+		fmt.Println("Perulangan ke ", i)
+		if i == 5 {
+			break
+		}
+	}
 }
