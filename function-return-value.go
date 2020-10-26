91f185a053cc4446e9f8b6419c82ea7664517b3b
muhammadzhuhry <muhammadzhuhry@gmail.com>
2020-10-26 15:36:38 +0700

    update file functino-return-value.go

--- function-return-value.go
+++ function-return-value.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import "fmt"
 
+func getHello(name string) string {
+	if name == "" {
+		return "Hallo, salam kenal!"
+	} else {
+		return "Hallo " + name
+	}
+}
+
+func main() {
+	fmt.Println(getHello(""))
+
+	result := getHello("Eko")
+	fmt.Println(result)
 }
