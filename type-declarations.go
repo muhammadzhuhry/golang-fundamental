4b7c13b93044a2c48de8f57af50ed440e00de441
muhammadzhuhry <muhammadzhuhry@gmail.com>
2020-10-26 22:29:22 +0700

    update file type-declarations.go

--- type-declarations.go
+++ type-declarations.go
@@ -10,3 +10,21 @@ func main() {
 	var marriedStatus Married = true
 	fmt.Println(noKtpEko, marriedStatus)
 }
+
+// integer
+
+// uint8 -> Unsigned 8-bit integers (0 to 255)
+// uint16-> Unsigned 16-bit integers (0 to 65535)
+// uint32-> Unsigned 32-bit integers (0 to 4294967295)
+// uint64-> Unsigned 64-bit integers (0 to 18446744073709551615)
+// int8 -> Unsigned 8-bit integers (-128 to 127)
+// int16-> Unsigned 16-bit integers (-32768 to 32767)
+// int32-> Unsigned 32-bit integers (-2147483648 to 2147483647)
+// int64-> Unsigned 64-bit integers (-9223372036854775808 to 9223372036854775807)
+
+// other
+
+// byte -> uint8
+// rune -> int32
+// uint -> uint32 or 64 bits(depend on computer)
+// int -> same as uint
