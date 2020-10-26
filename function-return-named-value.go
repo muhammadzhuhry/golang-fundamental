d5531b9d3a273a6f7ba4182d0147f49943031e6a
muhammadzhuhry <muhammadzhuhry@gmail.com>
2020-10-26 21:59:40 +0700

    update file function-return-named-value.go

--- function-return-named-value.go
+++ function-return-named-value.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import "fmt"
 
+func getFullName2() (firstName, middleName, lastName string) {
+	firstName, middleName, lastName = "Muhammad", "Athallah", "Zhuhry"
+	return
+}
+
+func main() {
+	first, middle, last := getFullName2()
+	fmt.Println(first, middle, last)
 }
