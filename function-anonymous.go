db43a353a50328c2279b9baf7e2a7b2869eaa34c
muhammadzhuhry <muhammadzhuhry@gmail.com>
2020-10-27 21:03:32 +0700

    update file function-anonymous.go

--- function-anonymous.go
+++ function-anonymous.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import "fmt"
 
+type Blacklist func(string) bool
+
+func registerUser(name string, blacklist Blacklist) {
+	if blacklist(name) {
+		fmt.Println("You are Blocked", name)
+	} else {
+		fmt.Println("Welcome", name)
+	}
+}
+
+//func blacklistAdmin(name string) bool {
+//	return name == "Admin"
+//}
+//
+//func blacklistRoot(name string) bool {
+//	return name == "Root"
+//}
+
+func main() {
+	//registerUser("Admin", blacklistAdmin)
+
+	// contoh anonymous function di variable
+	blacklistAdmin := func(name string) bool {
+		return name == "Admin"
+	}
+	registerUser("Admin", blacklistAdmin)
+
+	// contoh anonymous function di parameter
+	registerUser("Root", func(name string) bool {
+		return name == "Root"
+	})
 }
