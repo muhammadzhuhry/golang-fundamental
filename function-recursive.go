bf0fe8cbc656449d7c9c9384e36660a936cfb8a6
muhammadzhuhry <muhammadzhuhry@gmail.com>
2020-10-27 21:19:48 +0700

    update file function-recursive.go

--- function-recursive.go
+++ function-recursive.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import "fmt"
 
+// Recursive function adalah function yang memanggil dirinya sendiri
+// Contoh kasus yang lebih mudah menggunakan recursive function adalah faktorial
+
+func factorialLoop(value int) (result int) {
+	result = 1
+
+	for i := value; i > 0; i-- {
+		result *= i
+	}
+	return
+}
+
+func factorialRecursive(value int) int {
+	if value == 1 {
+		return 1
+	} else {
+		return value * factorialRecursive(value-1)
+	}
+}
+
+func main() {
+	loop := factorialLoop(5)
+	fmt.Println(loop)
+
+	recursive := factorialRecursive(5)
+	fmt.Println(recursive)
 }
