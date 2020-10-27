53394b6d61ee178f37b250cfed6e2d43677b43de
muhammadzhuhry <muhammadzhuhry@gmail.com>
2020-10-27 21:42:04 +0700

    update file closures.go

--- closures.go
+++ closures.go
@@ -1,7 +1,24 @@
 package main
 
-//closures adalah kemampuan sebuah function berinteraksi data data disekitarnya dalam scope yg sama
+import "fmt"
+
+//closures adalah kemampuan sebuah function berinteraksi dgn data data disekitarnya dalam scope yg sama
+
+//func increment(value int) int {
+//	return value + 1
+//}
 
 func main() {
+	//fmt.Println(increment(0))
 
+	counter := 0
+
+	increment := func() {
+		fmt.Println("Increment")
+		counter++
+	}
+
+	increment()
+	increment()
+	fmt.Println(counter)
 }
