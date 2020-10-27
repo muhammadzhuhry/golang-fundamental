416d71a1aa24c3cd456be604d55f63ab269a7185
muhammadzhuhry <muhammadzhuhry@gmail.com>
2020-10-27 19:31:35 +0700

    update file function-as-value.go

--- function-as-value.go
+++ function-as-value.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import "fmt"
 
+//function adalah first class citizen -> bisa berdiri sendiri
+//function juga merupakan tipe data dan bisa disimpan di dalam vairable
+
+func getGoodBye(name string) string {
+	return "Good Bye, " + name
+}
+
+func main() {
+	sayGoodBye := getGoodBye
+
+	result := sayGoodBye("Zuhri")
+
+	fmt.Println(result)
+	fmt.Println(sayGoodBye("Bayu"))
+	fmt.Println(getGoodBye("Herlambang"))
 }
