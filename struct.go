5833ab1ca4e6884ce7556471271ac3519da1456f
muhammadzhuhry <muhammadzhuhry@gmail.com>
2020-11-03 22:07:23 +0700

    update file struct.go

--- struct.go
+++ struct.go
@@ -1,5 +1,10 @@
 package main
 
+//Struct adalah sebuah template data yang digunakan untuk menggabungkan nol atau lebih tipe data lainnya dalam satu kesatuan
+//Struct biasanya representasi data dalam program aplikasi yang kita buat
+//Data di struct disimpan di field
+//Sederhananya struct adalah kumpulan dari field
+
 func main() {
 
 }
