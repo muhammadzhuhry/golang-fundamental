01bfebdba30a8e7a211be334373b8339687098e4
muhammadzhuhry <muhammadzhuhry@gmail.com>
2020-11-03 09:46:24 +0700

    update file recover.go

--- recover.go
+++ recover.go
@@ -1,8 +1,28 @@
 package main
 
+import "fmt"
+
 //Recover adalah function yang bisa kita gunakan untuk menangkap data panic
 //Dengan recover proses panic akan terhenti, sehingga program akan tetap berjalan
 
-func main() {
+func endApps() {
+	message := recover()
+	if message != nil {
+		fmt.Println("error message:", message)
+	}
+	fmt.Println("end app")
+}
 
+func runApps(error bool) {
+	defer endApps()
+	if error {
+		panic("APP ERROR")
+	}
+	fmt.Println("app running properly")
+}
+
+func main() {
+	//runApps(false)
+	runApps(true)
+	fmt.Println("app still running")
 }
