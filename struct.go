3dd6e86ec11034cb3244b1814843e99c3876a6f4
muhammadzhuhry <muhammadzhuhry@gmail.com>
2020-11-03 22:13:28 +0700

    update file struct.go

--- struct.go
+++ struct.go
@@ -1,10 +1,23 @@
 package main
 
+import "fmt"
+
 //Struct adalah sebuah template data yang digunakan untuk menggabungkan nol atau lebih tipe data lainnya dalam satu kesatuan
 //Struct biasanya representasi data dalam program aplikasi yang kita buat
 //Data di struct disimpan di field
 //Sederhananya struct adalah kumpulan dari field
 
-func main() {
+type Customer struct {
+	Name, Address string
+	Age           int
+}
 
+func main() {
+	var zuhri Customer
+
+	zuhri.Name = "Muhammad Athallah Zhuhry"
+	zuhri.Address = "Indonesia"
+	zuhri.Age = 23
+
+	fmt.Println(zuhri)
 }
