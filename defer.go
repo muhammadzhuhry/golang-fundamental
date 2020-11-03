f20e9f85b13b3fbc2b89175a3e725d938d997ccb
muhammadzhuhry <muhammadzhuhry@gmail.com>
2020-11-03 09:16:34 +0700

    update file defer.go

--- defer.go
+++ defer.go
@@ -1,8 +1,41 @@
 package main
 
+import "fmt"
+
 // Defer function adalah function yang bisa kita jadwalkan untuk dieksekusi setelah sebuah function selesai di eksekusi
 // Defer function akan selalu dieksekusi walaupun terjadi error di function yang di eksekusi
 
-func main() {
+// Ex without Defer func logging cannot be execute because of error on result
+//func logging() {
+//	fmt.Println("selesai memanggil function")
+//}
+//
+//func runApplication(number int) {
+//	fmt.Println("run application")
+//
+//	result := 10 / number
+//	fmt.Println(result)
+//
+//	logging()
+//}
+//
+//func main() {
+//	runApplication(0)
+//}
+//
+//// Ex with Defer func logging executed even though there is an error
+func logging() {
+	fmt.Println("selesai memanggil function")
+}
 
+func runApplication(number int) {
+	defer logging()
+	fmt.Println("run application")
+
+	result := 10 / number
+	fmt.Println(result)
+}
+
+func main() {
+	runApplication(0)
 }
