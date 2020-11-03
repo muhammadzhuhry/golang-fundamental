6c61dbc6337e9bccda109484d83c9efd4ce29e5e
muhammadzhuhry <muhammadzhuhry@gmail.com>
2020-11-03 09:32:34 +0700

    update file recover.go

--- recover.go
+++ recover.go
@@ -1,5 +1,8 @@
 package main
 
+//Recover adalah function yang bisa kita gunakan untuk menangkap data panic
+//Dengan recover proses panic akan terhenti, sehingga program akan tetap berjalan
+
 func main() {
 
 }
