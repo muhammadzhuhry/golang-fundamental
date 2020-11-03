5c7e722be43178a5ce2dbaeaf20e4dfb4066f145
muhammadzhuhry <muhammadzhuhry@gmail.com>
2020-11-03 09:06:22 +0700

    create file defer.go

---
+++ defer.go
@@ -0,0 +1,8 @@
+package main
+
+// Defer function adalah function yang bisa kita jadwalkan untuk dieksekusi setelah sebuah function selesai di eksekusi
+// Defer function akan selalu dieksekusi walaupun terjadi error di function yang di eksekusi
+
+func main() {
+
+}
