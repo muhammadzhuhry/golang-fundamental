ffc281bc4aed220a5b6d8a33c472f0a64322f2b9
muhammadzhuhry <muhammadzhuhry@gmail.com>
2020-11-05 15:23:29 +0700

    update file interface-kosong.go

--- interface-kosong.go
+++ interface-kosong.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import "fmt"
 
+// Interface kosong adalah interface yang tidak memiliki deklarasi method satupun, hal ini membuat secara otomatis	semua tipe data akan menjadi implementasinya
+
+/*cara membuat interface kosong -> interface{} atau sama dengan
+type Apapun interface {
+
+}*/
+
+func Ups(i int) interface{} {
+	if i == 1 {
+		return 1
+	} else if i == 2 {
+		return true
+	} else {
+		return "ups"
+	}
+}
+
+func main() {
+	//data := Ups(2)
+	var data interface{} = Ups(1)
+	fmt.Println(data)
 }
