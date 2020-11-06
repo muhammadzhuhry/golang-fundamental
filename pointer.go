5a3c5b59a3c2364a3c7122e3aa482baa7c4371b4
muhammadzhuhry <muhammadzhuhry@gmail.com>
2020-11-06 19:35:01 +0700

    update file pointer.go

--- pointer.go
+++ pointer.go
@@ -1,5 +1,8 @@
 package main
 
+// Secara default di golang semua variable itu pass by value, bukan by reference
+// Artinya, jika kita mengirim sebuah variable ke dalam function, method atau variable lain, sebenarnya yg dikirim adalah duplikasi value nya
+
 func main() {
 
 }
