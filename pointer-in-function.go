66461c1fa35b50e20cb5ecf6aa91ec0438b920d1
muhammadzhuhry <muhammadzhuhry@gmail.com>
2020-11-06 20:19:14 +0700

    update file pointer-in-function.go

--- pointer-in-function.go
+++ pointer-in-function.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import "fmt"
 
+// Saat kita membuat parameter di function, secara default adalah pass by value, artinya data akan di copy lalu dikirim ke function
+// Oleh karena itu, jika kita ingin mengubah data di dalam function, data yg asli tidak akan pernah berubah
+// Hal ini membuat variable menjadi aman, karena tidak akan bisa dirubah
+// Namun ada beberapa case yg kadang mengharuskan kita untuk membuat function yg bisa merubah data asli parameter tersebut
+// Untuk melakukan ini, kita juga bisa menggunakan pointer di function
+// Untuk menjadikan sebuah paramater sebagai pointer, kita bisa menggunakan operator * di parameternya
+
+type Alamat struct {
+	City, Province, Country string
+}
+
+func changeCountryToIndonesia(alamat *Alamat) {
+	alamat.Country = "Indonesia"
+}
+
+func main() {
+	adrres := Alamat{"Subang", "Jawa Barat", ""}
+	changeCountryToIndonesia(&adrres)
+
+	fmt.Println(adrres)
 }
