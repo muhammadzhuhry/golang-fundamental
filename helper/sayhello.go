2e810979bd9fa7287383f7d555387a407cd41cef
muhammadzhuhry <muhammadzhuhry@gmail.com>
2020-11-07 18:14:51 +0700

    update with access modifier example

--- helper/sayhello.go
+++ helper/sayhello.go
@@ -2,6 +2,14 @@ package helper
 
 import "fmt"
 
+var version = 1             // tidak bisa diakses file lain
+var Application = "Go-Lang" // bisa diakses file lain
+
+// karena  diawali huruf besar jadi ini bisa diakses dari file lain
 func SayHello(name string) {
 	fmt.Println("Hello", name)
+}
+
+func sayGoodbye(name string) {
+	fmt.Println("Goodbye", name)
 }
--- import.go
+++ import.go
@@ -1,7 +1,17 @@
 package main
 
-import "github.com/muhammadzhuhry/belajar-golang-dasar/helper"
+import (
+	"fmt"
+	"github.com/muhammadzhuhry/belajar-golang-dasar/helper"
+)
 
 func main() {
 	helper.SayHello("Zuhri")
+	//helper.sayGoodbye("Zuhri") // error
+	fmt.Println(helper.Application)
+	//fmt.Println(helper.version) // error
 }
+
+// ACCESS MODIFIER
+// Di bahasa pemrograman lain, biasanya ada kata kunci yg bisa digunakan untuk menentukan access modifier terhadap suatu function atau variable
+// Di Golang untuk menentukan access modifier, cukup menggunakan nama function atau variable(jika diawali dgn huruf besar maka artinya bisa di akses di package lain)
