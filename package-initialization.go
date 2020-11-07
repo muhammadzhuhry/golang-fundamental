17ff1ec45737712a4d1936914d4f056e2f0fd9a7
muhammadzhuhry <muhammadzhuhry@gmail.com>
2020-11-07 18:22:39 +0700

    update file package-initialization.go

--- package-initialization.go
+++ package-initialization.go
@@ -1,5 +1,9 @@
 package main
 
+// Saat kita membuat package, kita bisa membuat sebuah function yg akan di akses pertama kali ketika package kita di akses
+// ini sangat cocok ketika contohnya pacage kita berisi function-function untuk berkomunikasi dgn database, kita membuat  function inisialisasi utk membuat koneksi ke db
+// Untuk membuat function yg diakses secara otomatis ketika package diakses, kita cukup membuat function dgn nama init
+
 func main() {
 
 }
