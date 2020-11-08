3ad1b65c95925e04cf4f7f4ff9b49e5b89af02e7
muhammadzhuhry <muhammadzhuhry@gmail.com>
2020-11-08 19:16:51 +0700

    update file reflect.go

--- reflect.go
+++ reflect.go
@@ -1,5 +1,8 @@
 package main
 
+//  Salah satu fungsi reflect ialah dpt melihat struktur kode kita pd saat aplikasi sedang berjalan / tercompile
+// Reflection sgt berguna ketika kita ingin membuat library yg general sehingga mudah digunakan
+
 func main() {
 
 }
