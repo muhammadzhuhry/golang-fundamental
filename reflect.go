828a12c942da57de72a55a82c6c34b4bc358e862
muhammadzhuhry <muhammadzhuhry@gmail.com>
2020-11-08 19:37:05 +0700

    update file reflect.go

--- reflect.go
+++ reflect.go
@@ -1,8 +1,43 @@
 package main
 
+import (
+	"fmt"
+	"reflect"
+)
+
 //  Salah satu fungsi reflect ialah dpt melihat struktur kode kita pd saat aplikasi sedang berjalan / tercompile
 // Reflection sgt berguna ketika kita ingin membuat library yg general sehingga mudah digunakan
 
-func main() {
+type Sample struct {
+	Name string `required:"true" max:"10"`
+}
 
+func IsValid(data interface{}) bool {
+	t := reflect.TypeOf(data)
+
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		if field.Tag.Get("required") == "true" {
+			value := reflect.ValueOf(data).Field(i).Interface()
+
+			if value == "" {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func main() {
+	sample := Sample{"Eko"}
+
+	var sampleType reflect.Type = reflect.TypeOf(sample)
+
+	fmt.Println(sampleType.NumField())
+	fmt.Println(sampleType.Field(0).Name)
+	fmt.Println(sampleType.Field(0).Tag.Get("max"))
+
+	fmt.Println(IsValid(sample))
+	sample.Name = ""
+	fmt.Println(IsValid(sample))
 }
